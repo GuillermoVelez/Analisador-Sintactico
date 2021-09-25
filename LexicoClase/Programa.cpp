41ef61d5f08906669a97b4e2dd6d69fb61f1ec9a
ChristianGalindo10 <cygc1@hotmail.com>
2021-09-24 20:45:22 -0500

    Finalizado

---
+++ LexicoClase/Programa.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+int main(){
+int a=0;
+int b=0;
+printf("Al",b);
+if(a==1){
+ b=5;
+}
+printf("AMimir",b);
+system("pause");
+return b;
+}
